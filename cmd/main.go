910170c80ea5a0a93e9f764a7920323f678e024a
Daniil Benger <torigetz@yandex.ru>
2025-02-18 23:51:25 +0300

    feat: add auth handlers, connect to bd'

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	config "url-shortener/configs"
+	"url-shortener/internal/auth"
+	"url-shortener/pkg/db"
+)
+
 func main() {
-	panic("@todo")
+	cfg := config.LoadConfig()
+
+	_ = db.NewDb(*cfg)
+
+	router := http.NewServeMux()
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: cfg,
+	})
+
+	server := http.Server{
+		Addr:    cfg.App.Addr,
+		Handler: router,
+	}
+
+	fmt.Println("App listen at ", cfg.App.Addr)
+	server.ListenAndServe()
 }
--- configs/config.go
+++ configs/config.go
@@ -1,9 +1,44 @@
 package config
 
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
-	Db DbConfig
+	App  AppConfig
+	Db   DbConfig
+	Auth AuthConfig
 }
 
+type AppConfig struct {
+	Addr string
+}
 type DbConfig struct {
 	Dsn string
+}
+
+type AuthConfig struct {
+	Secret string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic("Unable to load .env")
+	}
+
+	return &Config{
+		App: AppConfig{
+			Addr: os.Getenv("APP_ADDR"),
+		},
+		Db: DbConfig{
+			Dsn: os.Getenv("DB_DSN"),
+		},
+		Auth: AuthConfig{
+			Secret: os.Getenv("AUTH_SECRET"),
+		},
+	}
 }
